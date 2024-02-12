684101dd3440726d29e9873d6d6f42d59739a9cc
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-02-12 23:40:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 //your JS code here. If required.
-const student {
-	name: "Ayush";
-}
+// const student {
+// 	name: "Ayush";
+// }
 
-Object.prototype.getKeys = function () {
-	return Object.keys(this);
-}
+// Object.prototype.getKeys = function () {
+// 	return Object.keys(this);
+// }
 
-console.log(student.getKeys());
+// console.log(student.getKeys());
+
+// Create an object called student with a property called name
+const student = {
+  name: 'John'
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+  // Get all the keys of the object and return them as an array
+  return Object.keys(this);
+};
+
+// Test the getKeys() method
+console.log(student.getKeys()); // Output: ['name']
